c966e072188095fb42d25d823700840975d6d13a
Matt307082 <125820319+Matt307082@users.noreply.github.com>
2026-08-20 13:15:29 +0000

    Deploying to gh-pages from @ abcdesktopio/docs@c1ad9f76eb9a6b5e06a2699fd105c62cf775dd50 🚀

--- js/drawio-reload.js
+++ js/drawio-reload.js
@@ -1,3 +1,22 @@
-document$.subscribe(({ body }) => {
-  GraphViewer.processElements()
-})
+(function () {
+  var loaded = false;
+
+  function renderDiagrams() {
+    GraphViewer.processElements();
+  }
+
+  function loadViewerScript(callback) {
+    if (loaded) { callback(); return; }
+    var s = document.createElement('script');
+    s.src = 'https://viewer.diagrams.net/js/viewer-static.min.js';
+    s.onload = function () { loaded = true; callback(); };
+    document.head.appendChild(s);
+  }
+
+  document$.subscribe(function (_ref) {
+    var body = _ref.body;
+    if (body.querySelector('.mxgraph, [data-mxgraph]')) {
+      loadViewerScript(renderDiagrams);
+    }
+  });
+})();
